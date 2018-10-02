176b0f12c896d8db0a22647e82b3895aa7b5adae
saleemmalikraja@gmail.com <saleemmalikraja@gmail.com>
2018-10-02 18:09:18 +0530

    dumb card component

--- src/app/shared/dumb-card/dumb-card.module.ts
+++ src/app/shared/dumb-card/dumb-card.module.ts
@@ -1,10 +1,29 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DumbCardComponent } from './dumb-card.component';
-
+import { FormsModule } from '@angular/forms';
+import {
+  MatFormFieldModule,
+  MatSelectModule,
+  MatInputModule,
+  MatButtonModule,
+  MatCardModule,
+  MatIconModule,
+  MatChipsModule,
+  MatExpansionModule
+} from '@angular/material';
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatSelectModule,
+    MatCardModule,
+    MatIconModule,
+    MatChipsModule,
+    MatExpansionModule
   ],
   declarations: [DumbCardComponent]
 })
