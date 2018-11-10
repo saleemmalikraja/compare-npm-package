b507a3027927f212c73b9e023605528532ca0808
saleemmalikraja@gmail.com <saleemmalikraja@gmail.com>
2018-11-10 11:42:16 +0530

    updated package json

--- src/environments/version.ts
+++ src/environments/version.ts
@@ -2,21 +2,21 @@
 /* tslint:disable */
 export const VERSION = {
     "dirty": true,
-    "raw": "v1.0.4-0-g5fd0dda-dirty",
-    "hash": "g5fd0dda",
+    "raw": "v1.0.5-0-g1ca9260-dirty",
+    "hash": "g1ca9260",
     "distance": 0,
-    "tag": "v1.0.4",
+    "tag": "v1.0.5",
     "semver": {
-        "raw": "v1.0.4",
+        "raw": "v1.0.5",
         "major": 1,
         "minor": 0,
-        "patch": 4,
+        "patch": 5,
         "prerelease": [],
         "build": [],
-        "version": "1.0.4"
+        "version": "1.0.5"
     },
-    "suffix": "0-g5fd0dda-dirty",
-    "semverString": "1.0.4",
+    "suffix": "0-g1ca9260-dirty",
+    "semverString": "1.0.5",
     "version": "0.0.0"
 };
 /* tslint:enable */
