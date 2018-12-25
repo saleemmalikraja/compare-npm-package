d7b60b46537e23654a05fabd2a38630f4caebb7c
saleemmalikraja@gmail.com <saleemmalikraja@gmail.com>
2018-12-25 17:08:47 +0530

    Revert "bug fixes in dev board view"
    This reverts commit fdd04b56b1e3bd5295a3c978ed790711042e1ac5.

--- src/app/shared/dumb-search/dumb-search.component.ts
+++ src/app/shared/dumb-search/dumb-search.component.ts
@@ -19,19 +19,19 @@ export class DumbSearchComponent implements OnInit {
 
   filterSource(source) {
     console.log(source);
-    const config = {
+    var config = {
       method: 'GET',
       apiUrl: 'apiUrlForNpm',
       endPoint: 'downloads/range/2018-9-03:2018-10-02/@angular/core'
-    };
+    }
 
     this.appService.apiRequest(config).subscribe((data) => {
 
     },
       error => {
-        console.error('Error filtering  source!');
+        console.error("Error saving food!");
         return throwError(error);  // Angular 5/RxJS 5.5
-      });
+      })
   }
 
   getnewsSources() {
--- src/readme.txt
+++
@@ -1,5 +0,0 @@
-#TO PUBLISH :
-
-1) Release the project using latest commit - through github
-
-2) Take build using 'npm run build:browser:prod' - will get the version from git and update the same in build
