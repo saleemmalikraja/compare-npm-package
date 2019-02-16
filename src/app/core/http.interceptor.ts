2ccd8874fe19b06f3dc3d4d97d628631b6f8a0e1
saleemmalikraja@gmail.com <saleemmalikraja@gmail.com>
2019-02-16 20:18:01 +0530

    Added Http Event type to trace the request

--- src/app/core/http.interceptor.ts
+++ src/app/core/http.interceptor.ts
@@ -5,6 +5,7 @@ import {
     HttpRequest,
     HttpEvent,
     HttpResponse,
+	HttpEventType,
     HttpErrorResponse
 } from '@angular/common/http';
 import { tap, finalize } from 'rxjs/operators';
@@ -30,6 +31,20 @@ export class PackageCompareInterceptor implements HttpInterceptor {
         return next.handle(duplicate) // passing control to the next interceptor in the chain
             .pipe(
                 tap((event: HttpEvent<any>) => {
+				 switch (event.type) {
+        case HttpEventType.Sent:
+          console.log('Request sent!');
+          break;
+        case HttpEventType.ResponseHeader:
+          console.log('Response header received!');
+          break;
+        case HttpEventType.DownloadProgress:
+          const kbLoaded = Math.round(event.loaded / 1024);
+          console.log(`Download in progress! ${ kbLoaded }Kb loaded`);
+          break;
+        case HttpEventType.Response:
+          console.log('😺 Done!', event.body);
+      }
                     if (event instanceof HttpResponse) {
                         // this.turnOffModal();
                     }
